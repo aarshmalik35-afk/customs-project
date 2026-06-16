f33f35ded1a8701004ee8a009d6ded8d843b32ad
Aarsh Malik <aarshmalik35@gmail.com>
2026-06-16 14:08:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
 import * as THREE from "https://unpkg.com/three@0.160.0/build/three.module.js?module";
+import { GLTFLoader } from "https://unpkg.com/three@0.160.0/examples/jsm/loaders/GLTFLoader.js?module";
 
 const scene = new THREE.Scene();
+scene.background = new THREE.Color(0x0b1f3a);
 
 const camera = new THREE.PerspectiveCamera(
 75,
-window.innerWidth/window.innerHeight,
+window.innerWidth / window.innerHeight,
 0.1,
 1000
 );
 
-camera.position.z = 5;
+camera.position.set(0, 20, 50);
 
-const renderer = new THREE.WebGLRenderer();
+const renderer = new THREE.WebGLRenderer({
+antialias: true
+});
 
 renderer.setSize(
 window.innerWidth,
@@ -22,21 +26,62 @@ document
 .getElementById("container")
 .appendChild(renderer.domElement);
 
-const cube = new THREE.Mesh(
-new THREE.BoxGeometry(),
-new THREE.MeshNormalMaterial()
+const ambient = new THREE.AmbientLight(
+0xffffff,
+5
 );
 
-scene.add(cube);
+scene.add(ambient);
+
+const loader = new GLTFLoader();
+
+loader.load(
+
+'ship.glb',
+
+function(gltf){
+
+const ship = gltf.scene;
+
+scene.add(ship);
+
+console.log("SHIP LOADED");
+
+console.log(ship);
+
+ship.position.set(
+0,
+0,
+0
+);
+
+// MASSIVE SCALE
+ship.scale.set(
+100,
+100,
+100
+);
+
+},
+
+undefined,
+
+function(error){
+
+console.error(error);
+
+}
+
+);
 
 function animate(){
 
 requestAnimationFrame(animate);
 
-cube.rotation.x += 0.01;
-cube.rotation.y += 0.01;
-
-renderer.render(scene,camera);
+renderer.render(
+scene,
+camera
+);
 
 }
 
